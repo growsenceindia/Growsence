3d65c33408f2ce0b462923633dfffb31dbfcd0be
Growsence <teamgrowsence@gmail.com>
2025-07-05 13:10:52 +0530

    Update page.js

--- growsence-main/app/page.js
+++ growsence-main/app/page.js
@@ -1,34 +1,36 @@
 'use client';
-import HeroSection from './components/HeroSection';
-import HowItWorksSection from './components/HowItWorksSection';
-import FooterSection from './components/FooterSection';
-import AboutSection from "./components/AboutSection";
-import WhyChooseUsSection from "./components/WhyChooseUsSection";
-import TestimonialsSection from "./components/TestimonialsSection";
-import StatsCounterSection from "./components/StatsCounterSection";
-import ScrollProgressBar from "./components/ScrollProgressBar";
-import FAQSection from "./components/FAQSection";
-import CourseSlider from "./components/CourseSlider";
-import LegalCertificatesSection from "./components/LegalCertificatesSection";
-import CoursePricingSection from "./components/CoursePricingSection";
-import Navbar from "./components/Navbar";
-import { FaWhatsapp } from 'react-icons/fa'
+import { FaWhatsapp } from 'react-icons/fa';
+// import HeroSection from './components/HeroSection';
+// import HowItWorksSection from './components/HowItWorksSection';
+// import FooterSection from './components/FooterSection';
+// import AboutSection from "./components/AboutSection";
+// import WhyChooseUsSection from "./components/WhyChooseUsSection";
+// import TestimonialsSection from "./components/TestimonialsSection";
+// import StatsCounterSection from "./components/StatsCounterSection';
+// import ScrollProgressBar from "./components/ScrollProgressBar";
+// import FAQSection from "./components/FAQSection";
+// import CourseSlider from "./components/CourseSlider";
+// import LegalCertificatesSection from "./components/LegalCertificatesSection";
+// import CoursePricingSection from "./components/CoursePricingSection';
+// import Navbar from "./components/Navbar";
+
 export default function Home() {
   return (
     <main className="bg-white text-gray-800">
-      <Navbar />
-      <ScrollProgressBar />
-      <CourseSlider />
-      <HeroSection />
-      <AboutSection />
-      <StatsCounterSection />
-      <CoursePricingSection />
-      <HowItWorksSection />
-      <WhyChooseUsSection />
-      <TestimonialsSection />
-      <LegalCertificatesSection />
-      <FAQSection />
-      <FooterSection />
+      {/* <Navbar /> */}
+      {/* <ScrollProgressBar /> */}
+      {/* <CourseSlider /> */}
+      {/* <HeroSection /> */}
+      {/* <AboutSection /> */}
+      {/* <StatsCounterSection /> */}
+      {/* <CoursePricingSection /> */}
+      {/* <HowItWorksSection /> */}
+      {/* <WhyChooseUsSection /> */}
+      {/* <TestimonialsSection /> */}
+      {/* <LegalCertificatesSection /> */}
+      {/* <FAQSection /> */}
+      {/* <FooterSection /> */}
+
       {/* WhatsApp Floating Button */}
       <a
         href="https://wa.me/919835742586"
