68c3422741a46a7f61a82fc7307809cbbdfd65f7
Growsence <teamgrowsence@gmail.com>
2025-07-05 06:34:12 +0000

    Removed firebase integration

--- growsence-main/app/components/HeroSection.js
+++ growsence-main/app/components/HeroSection.js
@@ -2,7 +2,6 @@
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
-import { auth } from "../../lib/firebase";
 import {
   GoogleAuthProvider,
   signInWithPopup,
