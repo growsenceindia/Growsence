16a6e62ae2cdb1f7ef95f9b26a381de653865515
Growsence <teamgrowsence@gmail.com>
2025-07-05 13:52:15 +0530

    Update page.js

--- growsence-main/app/page.js
+++ growsence-main/app/page.js
@@ -1,36 +1,62 @@
 'use client';
+import Navbar from './components/Navbar';
+import ScrollProgressBar from './components/ScrollProgressBar';
+import CourseSlider from './components/CourseSlider';
+import HeroSection from './components/HeroSection';
+import AboutSection from './components/AboutSection';
+import StatsCounterSection from './components/StatsCounterSection';
+import CoursePricingSection from './components/CoursePricingSection';
 import HowItWorksSection from './components/HowItWorksSection';
+import WhyChooseUsSection from './components/WhyChooseUsSection';
+import TestimonialsSection from './components/TestimonialsSection';
+import LegalCertificatesSection from './components/LegalCertificatesSection';
+import FAQSection from './components/FAQSection';
 import FooterSection from './components/FooterSection';
-import AboutSection from "./components/AboutSection";
-import WhyChooseUsSection from "./components/WhyChooseUsSection";
-import TestimonialsSection from "./components/TestimonialsSection";
-import StatsCounterSection from "./components/StatsCounterSection";
-import ScrollProgressBar from "./components/ScrollProgressBar";
-import FAQSection from "./components/FAQSection";
-import CourseSlider from "./components/CourseSlider";
-import LegalCertificatesSection from "./components/LegalCertificatesSection";
-import CoursePricingSection from "./components/CoursePricingSection";
-import Navbar from "./components/Navbar";
 import { FaWhatsapp } from 'react-icons/fa';
 
 export default function Home() {
   return (
     <main className="bg-white text-gray-800">
+      {/* Top Navbar */}
       <Navbar />
+
+      {/* Scroll Progress */}
       <ScrollProgressBar />
+
+      {/* Course Slider */}
       <CourseSlider />
-      {/* <HeroSection />  ❌ Temporarily removed */}
+
+      {/* Hero Section */}
+      <HeroSection />
+
+      {/* About Growsence */}
       <AboutSection />
+
+      {/* Stats Section */}
       <StatsCounterSection />
+
+      {/* Course Pricing */}
       <CoursePricingSection />
+
+      {/* How It Works */}
       <HowItWorksSection />
+
+      {/* Why Choose Us */}
       <WhyChooseUsSection />
+
+      {/* Testimonials */}
       <TestimonialsSection />
+
+      {/* Legal Certificates */}
       <LegalCertificatesSection />
+
+      {/* FAQs */}
       <FAQSection />
+
+      {/* Footer */}
       <FooterSection />
-      
-      {/* WhatsApp Floating Button */}
+
+      {/* Floating WhatsApp Button */}
       <a
         href="https://wa.me/919835742586"
         target="_blank"
@@ -41,4 +67,4 @@ export default function Home() {
       </a>
     </main>
   );
-                          }
+    }
