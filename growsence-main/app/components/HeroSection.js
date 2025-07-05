a5d485728e3c53958af19b1da64685a357d88488
Growsence <teamgrowsence@gmail.com>
2025-07-05 08:36:13 +0000

    Integrated Firebase auth and fixed login modal

--- growsence-main/app/components/HeroSection.js
+++ growsence-main/app/components/HeroSection.js
@@ -1,6 +1,7 @@
 "use client";
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
+import { auth } from "@/lib/firebase";
 import Image from "next/image";
 import {
   GoogleAuthProvider,
---
+++ growsence-main/lib/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCPzp88ricpJHSuMJBP5EEuSPWEexEsc1g",
+  authDomain: "growsence-7bd93.firebaseapp.com",
+  projectId: "growsence-7bd93",
+  storageBucket: "growsence-7bd93.firebasestorage.app",
+  messagingSenderId: "74181205561",
+  appId: "1:74181205561:web:11234010a41a25b5c977e8",
+  measurementId: "G-4VCNG6KSLC"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
